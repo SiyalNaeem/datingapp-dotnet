1e016c2ee4c4147e40754bace6d86df03034b227
Naeem Ali <siyalnaeem1997@gmail.com>
2025-09-10 10:07:58 +0100

    implement repository pattern for members

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public required string PublicId { get; set; }
+    public bool IsMain { get; set; }
+    public string MemberId { get; set; } = null!;
+
+    [JsonIgnore]
+    public Member Member { get; set; } = null!;
+}
